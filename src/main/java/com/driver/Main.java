32d77cd7adc2cf9db4be415baf88794c0daf537a
sharukh12345 <sharufshaik0@gmail.com>
2023-07-19 14:24:13 +0530

    submit code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
-}
+    private String  setName;
+    private String getName;
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("John"); // Set the value of name using the setter method
+        System.out.println(obj.getName()); // Retrieve the value of name using the getter metho
+    }}
